c7e4b4173b2af71090ed1f502fc553dabf6d30dc
Ananda Bayu <ananda.bayu.tif418@polban.ac.id>
2021-10-07 15:08:47 +0700

    revision #1
    *add operator feature

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,7 +23,7 @@ const RESOURCES = {
 "assets/assets/images/twitteruser/totalcristiano.png": "a5d7c34c1f45d023796d6a3bb5be3396",
 "assets/FontManifest.json": "5a32d4310a6f5d9a6b651e75ba0d7372",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
-"assets/NOTICES": "b678f67b955dc6b328e43025f3b4f31b",
+"assets/NOTICES": "b0e229a71d2a604836ceacdfb62d3d44",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/packages/fluttertoast/assets/toastify.css": "a85675050054f179444bc5ad70ffc635",
 "assets/packages/fluttertoast/assets/toastify.js": "e7006a0a033d834ef9414d48db3be6fc",
@@ -33,9 +33,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "2cf2b723a43d87400ea830af679649c3",
-"/": "2cf2b723a43d87400ea830af679649c3",
-"main.dart.js": "1dd40f53f11eafc525b218275777c19f",
+"index.html": "7e9b77dac051359ef9ccba782ea54fa0",
+"/": "7e9b77dac051359ef9ccba782ea54fa0",
+"main.dart.js": "7ebcb91d786a61fcd0f7130d4ec80c87",
 "manifest.json": "94af3eb2cba4fdb7263f643df934a348",
 "version.json": "8da62812e186b57bd8c252e93631fc11"
 };
